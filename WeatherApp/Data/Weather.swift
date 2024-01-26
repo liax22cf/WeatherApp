d03d91cd6b33107c5834c8a02f8d3242cb020ebb
Axel Lindh <liax22cf@student.ju.se>
2024-01-26 10:45:23 +0100

    Initial Commit

---
+++ WeatherApp/Data/Weather.swift
@@ -0,0 +1,8 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Axel Lindh on 2024-01-26.
+//
+
+import Foundation
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Axel Lindh on 2024-01-26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
